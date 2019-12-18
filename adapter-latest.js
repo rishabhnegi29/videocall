8ec3866ea0e221e509951a4f6260138ba6f44b25
rishabhnegi29 <rishabhnegi29@gmail.com>
2019-12-18 12:20:17 +0530

    changed the adapte.js
    on 18 dec code copied form adapter.js of kurento 6.9

--- adapter-latest.js
+++ adapter-latest.js
@@ -3073,7 +3073,7 @@ function isActionAllowedInSignalingState(action, type, signalingState) {
 
 function maybeAddCandidate(iceTransport, candidate) {
   // Edge's internal representation adds some fields therefore
-  // not all fieldѕ are taken into account.
+  // not all fieldÑ• are taken into account.
   var alreadyAdded = iceTransport.getRemoteCandidates()
       .find(function(remoteCandidate) {
         return candidate.foundation === remoteCandidate.foundation &&
@@ -3475,7 +3475,7 @@ module.exports = function(window, edgeVersion) {
     iceGatherer.onlocalcandidate = function(evt) {
       if (pc.usingBundle && sdpMLineIndex > 0) {
         // if we know that we use bundle we can drop candidates with
-        // ѕdpMLineIndex > 0. If we don't do this then our state gets
+        // Ñ•dpMLineIndex > 0. If we don't do this then our state gets
         // confused since we dispose the extra ice gatherer.
         return;
       }
@@ -3483,7 +3483,7 @@ module.exports = function(window, edgeVersion) {
       event.candidate = {sdpMid: mid, sdpMLineIndex: sdpMLineIndex};
 
       var cand = evt.candidate;
-      // Edge emits an empty object for RTCIceCandidateComplete‥
+      // Edge emits an empty object for RTCIceCandidateCompleteâ€¥
       var end = !cand || Object.keys(cand).length === 0;
       if (end) {
         // polyfill since RTCIceGatherer.state is not implemented in
@@ -5244,7 +5244,7 @@ SDPUtils.parseRtcpParameters = function(mediaSection) {
   rtcpParameters.reducedSize = rsize.length > 0;
   rtcpParameters.compound = rsize.length === 0;
 
-  // parses the rtcp-mux attrіbute.
+  // parses the rtcp-mux attrÑ–bute.
   // Note that Edge does not support unmuxed RTCP.
   var mux = SDPUtils.matchPrefix(mediaSection, 'a=rtcp-mux');
   rtcpParameters.mux = mux.length > 0;
